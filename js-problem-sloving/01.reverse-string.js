f44984a70cb0a9880fb72dd073110e5189f574c3
habiburRhaman05 <devhabib2005@gmail.com>
2026-04-16 23:12:40 +0600

    sloved reverse-string problem

---
+++ js-problem-sloving/01.reverse-string.js
@@ -0,0 +1,29 @@
+// 1. Reverse a String
+// Task: Write a function that takes a string and returns it reversed.
+
+// Input: string s
+// Output: string
+// Constraints: s.length <= 1000.
+// Edge Case: Single character strings or empty strings.
+// Hint: Look into built-in string slicing or a simple loop.
+
+function reverseString(s){
+    let output = "";
+    let lastIndex = s.length - 1;
+
+    if (s.length === 0) return "empty string is not allow";
+    if (s.length === 1) return s
+    if (s.length > 1000) return "this text is too long";
+
+         for (let index = lastIndex; index >= 0; index--) {
+      output += s[index]
+    }
+    return output
+}
+
+
+
+  
+
+
+console.log(reverseString("hello"));
